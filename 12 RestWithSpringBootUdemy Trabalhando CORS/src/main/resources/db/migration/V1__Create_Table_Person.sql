703d412f1a8ad201af53ffa3890ae61ebb5a5c13
Almeida1234 <almeidasilva_rick@hotmail.com>
2022-01-25 15:10:44 -0300

    Removed folder from repository

--- 12 RestWithSpringBootUdemy Trabalhando CORS/src/main/resources/db/migration/V1__Create_Table_Person.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE rest_with_spring_boot_udemy.person (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `address` varchar(100) NOT NULL,
-  `first_name` varchar(80) NOT NULL,
-  `gender` varchar(10) NOT NULL,
-  `last_name` varchar(80) NOT NULL,
-  PRIMARY KEY (`id`)
-)
--- 12 RestWithSpringBootUdemy Trabalhando CORS/src/main/resources/db/migration/V3__Create_Table_Books.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE  rest_with_spring_boot_udemy.books (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `author` longtext,
-  `launch_date` datetime(6) NOT NULL,
-  `price` decimal(65,2) NOT NULL,
-  `title` longtext,
-  PRIMARY KEY (`id`)
-);
