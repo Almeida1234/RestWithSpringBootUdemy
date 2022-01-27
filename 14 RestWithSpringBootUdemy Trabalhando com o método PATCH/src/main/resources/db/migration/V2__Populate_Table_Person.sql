4823fe8fb888b899c545f3bf29895c129b4761e4
Almeida1234 <almeidasilva_rick@hotmail.com>
2022-01-26 22:19:01 -0300

    Trabalhando com método PATCH

---
+++ 14 RestWithSpringBootUdemy Trabalhando com o método PATCH/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO rest_with_spring_boot_udemy.person VALUES 
+	(1,'Barueri, Sao Paulo - Brasil','Ricardo','Masculino','Almeida'),
+	(2,'Barueri, Sao Paulo - Brasil','Cecilia','Feminina','Novaes'),
+	(3,'Barueri, Sao Paulo - Brasil','Camily','Feminina','Novaes'),
+	(4,'Barueri, Sao Paulo - Brasil','Alexandre','Masculino','Almeida'),
+	(7,'Barueri, Sao Paulo - Brasil','Lily','Feminina','Almeida Novaes'),
+	(8,'Barueri, Sao Paulo - Brasil','Cristal','Feminina','Almeida Novaes');
---
+++ 14 RestWithSpringBootUdemy Trabalhando com o método PATCH/src/main/resources/db/migration/V6__Insert_Data_In_Permission.sql
@@ -0,0 +1,4 @@
+INSERT INTO rest_with_spring_boot_udemy.permission VALUES 
+(1,'ADMIN'),
+(2,'MANAGER'),
+(3,'COMMON_USER');
