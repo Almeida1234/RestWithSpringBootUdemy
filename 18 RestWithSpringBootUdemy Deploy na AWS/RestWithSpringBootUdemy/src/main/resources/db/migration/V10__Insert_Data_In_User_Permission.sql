31fc6f889c5e5e98152770f12749f4a7df3e8ec9
Almeida1234 <almeidasilva_rick@hotmail.com>
2022-02-17 22:58:11 -0300

    Deploy na Amazon

---
+++ 18 RestWithSpringBootUdemy Deploy na AWS/RestWithSpringBootUdemy/src/main/resources/db/migration/V10__Insert_Data_In_User_Permission.sql
@@ -0,0 +1,4 @@
+INSERT INTO rest_with_spring_boot_udemy.user_permission VALUES 
+(1,1),
+(2,1),
+(1,2);
---
+++ 18 RestWithSpringBootUdemy Deploy na AWS/RestWithSpringBootUdemy/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -0,0 +1,3 @@
+INSERT INTO rest_with_spring_boot_udemy.users VALUES 
+(1,'Ricardo','Ricardo Almeida Novaes','$2a$16$9qr2tv0HmXbHBsx.TZFjfux742wCZM32a8Wi6iBqwIqaizlHPuxHS',_binary '',_binary '',_binary '',_binary ''),
+(2,'Cecilia','Cecilia Almeida Novaes','$2a$16$h4yDQCYTy62R6xrtFDWONeMH3Lim4WQuU/aj8hxW.dJJoeyvtEkhK',_binary '',_binary '',_binary '',_binary '');
